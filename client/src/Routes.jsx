7f3faf6bf849356d192043e30808dfdc2f19cd7a
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2023-09-03 14:19:26 +0530

    create logging and register page

---
+++ client/src/Routes.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import RegisterAndLoginForm from "./RegisterAndLoginForm.jsx";
+import {useContext} from "react";
+import {UserContext} from "./UserContext.jsx";
+import Chat from "./Chat";
+
+export default function Routes() {
+  const {username, id} = useContext(UserContext);
+
+  if (username) {
+    return <Chat />;
+  }
+
+  return (
+    <RegisterAndLoginForm />
+  );
+}
+
+
